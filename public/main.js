208bea5cc24ca23ce080ae46b2ae5b8ee94a341a
Pa-miu <gachaisbadciv@gmail.com>
2019-04-02 19:07:23 -0400

    Change load URL to refer to built index.html

--- public/main.js
+++ public/main.js
@@ -1,11 +1,13 @@
- const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
 
- function createWindow() {
- 	// Create app window
- 	const win = new BrowserWindow({width: 800, height: 600});
+function createWindow() {
+	// Create app window
+	const win = new BrowserWindow({width: 800, height: 600, icon: __dirname + '/assets/icon.png'});
 
- 	// Load index.html file
- 	win.loadURL('http://localhost:3000');
- }
+	// Load index.html file
+	win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
+	//win.loadURL('http://localhost:3000/');
+}
 
 app.on('ready', createWindow);
