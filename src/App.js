e5f0e97df8d8d91393f02d11b887df6ba66fe0f1
Pa-miu <gachaisbadciv@gmail.com>
2019-04-03 19:45:00 -0400

    Block out elements I will need

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import SingleInput from './SingleInput.js'
+import Placeholder from './Placeholder.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>fantasy drafter</h1>
-        <SingleInput id={'minsal'} name={'Minimum Salary'} placeholder={34000} />
-        <SingleInput id={'maxsal'} name={'Maximum Salary'} placeholder={35000} />
+        <div>
+        	<Placeholder name={'PROJECTION FILE'} feature={'load an external csv'}/>
+        </div>
+        <div>
+        	<SingleInput id={'minsal'} name={'Minimum Salary'} placeholder={34000} />
+        	<SingleInput id={'maxsal'} name={'Maximum Salary'} placeholder={35000} />
+        </div>
+        <div>
+        	<SingleInput id={'explineups'} name={'Number of Expected Lineups'} placeholder={100} />
+        	<SingleInput id={'minplyrdiff'} name={'Minimum Player Difference Between Lineups'} placeholder={3} />
+        </div>
+        <div>
+        	<Placeholder name={'USE EXPOSURES'} feature={'checkbox or dropdown boolean'}/>
+        </div>
+        <div>
+        	<Placeholder name={'Exclude Pitchers From'} feature={'Chosen-JS multiple select team list'}/>
+        	<Placeholder name={'Exclude Hitters From'} feature={'Chosen-JS multiple select team list'}/>
+        </div>
+        <div>
+					<Placeholder name={'Hitter Exposures'} feature={'Chosen-JS multiple select team list'}/>
+        </div>
       </div>
     );
   }
